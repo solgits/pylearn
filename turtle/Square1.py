c43768ace2cb095fa4e82e11f38594e466d265f1
shylix <shylix@naver.com>
2019-05-06 13:30:34 +0900

    [add] turtle samples

---
+++ turtle/Square1.py
@@ -0,0 +1,10 @@
+import turtle
+
+t = turtle.Pen()
+
+for x in range(100):
+    t.forward(x)
+    t.left(91)
+
+
+
---
+++ turtle/Square2.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Pen()
+
+t.forward(10)
+t.left(90)
+
+t.forward(10)
+t.left(90)
+
+t.forward(10)
+t.left(90)
+
+
