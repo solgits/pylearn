1cb58bd22fa0bbefeafff30b2ee0c2810528b3f9
seamusic <seamusic43@gmail.com>
2022-09-20 18:11:10 +0900

    [edit] Square2

--- turtle/Square2.py
+++ turtle/Square2.py
@@ -2,15 +2,15 @@ import turtle
 
 t = turtle.Pen()
 
-t.forward(100)
+t.forward(50)
 t.left(90)
 
-t.forward(100)
+t.forward(50)
 t.left(90)
 
-t.forward(100)
+t.forward(50)
 t.left(90)
 
-t.forward(100)
+t.forward(50)
 t.left(90)
 
