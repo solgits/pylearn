415ad7ef61f9a75ed3a99ee1da95a7e73999c248
GlaxyLord <okaysungnam@naver.com>
2022-12-24 23:17:05 +0900

    [edit] Square2.py

--- turtle/Square2.py
+++ turtle/Square2.py
@@ -2,15 +2,13 @@ import turtle
 
 t = turtle.Pen()
 
-t.forward(100)
+t.forward(10)
 t.left(90)
 
-t.forward(100)
+t.forward(10)
 t.left(90)
 
-t.forward(100)
+t.forward(10)
 t.left(90)
 
-t.forward(100)
-t.left(90)
 
